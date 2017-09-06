6f612cfc56395253da9512ee6eae4e91c3f75fd9
luhao <luhao@mogoroom.com>
2017-09-06 10:03:07 +0800

    Initial Commit

---
+++ MogoAirport/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MogoAirport
+//
+//  Created by luhao on 06/09/2017.
+//  Copyright © 2017 luhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MogoAirport/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MogoAirport
+//
+//  Created by luhao on 06/09/2017.
+//  Copyright © 2017 luhao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
